abb01ba757746cbcee3350e20a529b3ff6868422
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-11 21:23:30 +0200

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""sum_list function"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum_list function takes list of floats and returns sum"""
+    return sum(input_list)
