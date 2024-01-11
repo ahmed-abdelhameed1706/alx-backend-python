45aef9055fb647a49a3b24588b6a3ee343623793
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-11 21:00:14 +0200

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains the function add(a, b)"""
+
+
+def add(a: float, b: float) -> float:
+    """function takes floats and returns floats"""
+    return a + b
