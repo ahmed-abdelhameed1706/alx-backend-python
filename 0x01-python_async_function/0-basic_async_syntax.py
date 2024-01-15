d3250b25b4d6e9f80c3ecd8b57ab3fa17819e20d
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-15 22:15:46 +0200

    0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ import random
 
 
 async def wait_random(max_delay=10):
+    """function to await a number and return it"""
     await_number = random.uniform(0, 10)
     await asyncio.sleep(await_number)
     return await_number
