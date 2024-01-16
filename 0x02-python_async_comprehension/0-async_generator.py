6c33eee3e6b1d672eba9e820432cfa4eef938daa
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-16 23:33:44 +0200

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator:
+async def async_generator() -> Generator[float, None, None]:
     """async generator function"""
     for _ in range(10):
         await asyncio.sleep(1)
