7662e5f999a42c78b5111c960c7ba07e29fb9dc7
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-16 23:30:20 +0200

    0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""async generator function"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float]:
+    """async generator function"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
