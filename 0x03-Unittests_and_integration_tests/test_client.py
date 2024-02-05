14a05d9b03aca60d2d1006bc4bf697f2726d0bdb
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-05 14:05:13 +0200

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ module to test client """
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -23,6 +23,23 @@ class TestGithubOrgClient(unittest.TestCase):
         )  # no pep8
 
         self.assertEqual(response, {"name": org_name})
+
+    def test_public_repos_url(self):
+        """method to test puclic repos"""
+
+        mock_payload = {"repos_url": "url"}
+
+        with patch.object(
+            GithubOrgClient, "org", new_callable=PropertyMock
+        ) as mock_org:
+            mock_org.return_value = mock_payload
+
+            gh_client = GithubOrgClient("repos_url")
+            repos_url = gh_client._public_repos_url
+
+            result = "url"
+
+            self.assertEqual(repos_url, result)
 
 
 if __name__ == "__main__":
